fa56c73e73e8a19f4b6d96a126be375c6b62bbcb
tinyRickMotherfucker <ralcarazerhs@gmail.com>
2017-10-27 13:24:18 -0700

    Initial commit

---
+++ DataBackup.h
@@ -0,0 +1,22 @@
+#include "Arduino.h"
+#include "Stepper.h"
+#ifndef DataBackup_h
+#define DataBackup_h
+
+class DataBackup {
+	public:
+		void readVcc();
+		MazeValues mazeRestore();
+		MazeValues mazeBackup();
+		void lowPowerBackup();
+
+
+	private:
+		void EEPROM_write(uint16_t address, uint8_t data);
+		void EEPROM_read(uint16_t address, uint8_t data);
+		void vccReadSetup();
+
+
+};
+
+#endif
